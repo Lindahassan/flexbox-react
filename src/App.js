4b72029bb5484f2fa54fc022eb0f4195740e8fd2
linda <lindahassan122@gmail.com>
2022-01-24 22:18:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+     <div className="container-one">
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        
+     </div> 
+     <div  className='cont' >
+        <div className="container">
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        </div>
+        <div className="container-four">
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        
+     </div>
+     </div> 
+     <div className="container-tow">
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+            </div>
+           </div>
+       );
+}
+
+export default App;
